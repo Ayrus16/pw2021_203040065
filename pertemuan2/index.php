c8c948caa2c6904e281c7af6ef7f4c7d1db91e77
Ayrus16 <suryap.sp83@gmail.com>
2021-02-12 13:57:45 +0700

    Mempelajari sintaks dasar php

---
+++ pertemuan2/index.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Surya Putra Pratama
+203040065
+https://github.com/Ayrus16/pw2021_203040065
+Pertemuan 2 - 12 Februari 2021
+Mempelajari Sintaks dasar php, standar output, variable dan tipe data
+*/
+?>
+
+
+<?php
+// sintaks standar output
+// echo, print 
+// print_r  [biasanya untuk debugging]
+// var_dumb  [biasanya untuk debugging]
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Surya Putra Pratama";
+// echo "Halo, Nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung string / concatenation / concat
+// $nama_depan = "Surya";
+// $nama_belakang = "Putra";
+// echo $nama_depan . " " . $nama_belakang
+
+// Assigment
+// =, +=, *=, /=, %=, .= 
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Surya";
+// $nama .= " ";
+// $nama .= "Putra";
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !===
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// var_dump($x < 10 || $x % 2 == 0);
+?>
