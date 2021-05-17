7612bc6d0bccd0fa7534676c4c52aaab2ed7eefc
Ayrus16 <suryap.sp83@gmail.com>
2021-05-17 19:00:13 +0700

    Menambahakan fitur live search & upload gambar

---
+++ pertemuan13/ajax/ajax_cari.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Surya Putra Pratama
+203040065
+https://github.com/Ayrus16/pw2021_203040065
+Pertemuan 13 - 13 April 2021
+
+Menambah fitur live search dan upload gambar
+*/
+?>
+<?php
+require '../function.php';
+$mahasiswa = cari($_GET['keyword']);
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <th>#</th>
+    <th>Gambar</th>
+    <th>Nama</th>
+    <th>Aksi</th>
+  </tr>
+  <?php if (empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4">
+        <p style="color: red; font-style:italic; ">Data mahasiswa tidak ditemukan!</p>
+      </td>
+    </tr>
+  <?php endif; ?>
+
+  <?php
+  $i = 1;
+  foreach ($mahasiswa as $m) : ?>
+    <tr>
+      <td><?= $i++; ?></td>
+      <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
+      <td><?= $m['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $m['id'] ?>">Lihat detail</a>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+</table>
---
+++ pertemuan13/logout.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Surya Putra Pratama
+203040065
+https://github.com/Ayrus16/pw2021_203040065
+Pertemuan 13 - 13 April 2021
+
+Menambah fitur live search dan upload gambar
+*/
+?>
+<?php
+session_start();
+session_destroy();
+header('Location: login.php');
+exit;
+?>
