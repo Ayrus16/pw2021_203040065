939b5c4061fb71c01e8f4a1dcd124c1f1a03896e
Ayrus16 <suryap.sp83@gmail.com>
2021-05-28 15:11:49 +0700

    Update asset praktikum

--- praktikum/tugas6/Latihan6a/assets/Latihan3a.php
+++ praktikum/tugas6/Latihan6a/assets/Latihan3a.php
@@ -7,35 +7,39 @@ Shift Jum'at 10.00 - 11.00
 Modul - 3 Array
 */
 ?>
-<?php $tulisan = ["ada","abel","men","pung","pung","nilai"]; ?>
+<?php $tulisan = ["ada", "abel", "men", "pung", "pung", "nilai"]; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 3a | 203040065</title>
     <style>
-    p {
-        font-family: arial;
-        border: 1px solid black;
-        padding: 10px 20px;
-        padding-bottom: 50px;
-        margin : auto;
-        width: fit-content;
-    }
-    p span{
-        background-color: yellow;
-    }
+        p {
+            font-family: arial;
+            border: 1px solid black;
+            padding: 10px 20px;
+            padding-bottom: 50px;
+            margin: auto;
+            width: fit-content;
+        }
+
+        p span {
+            background-color: yellow;
+        }
     </style>
 </head>
+
 <body>
     <p>
-        Array <span><?= $tulisan[0] ?></span> 
-        suatu vari<span><?= $tulisan[1]?> </span> 
-        yang dapat 
-        <span><?= $tulisan[2]?></span>am<span><?= $tulisan[3]?></span>  
-        banyak <span><?= $tulisan[5]?></span> 
+        Array <span><?= $tulisan[0] ?></span>
+        suatu vari<span><?= $tulisan[1] ?> </span>
+        yang dapat
+        <span><?= $tulisan[2] ?></span>am<span><?= $tulisan[3] ?></span>
+        banyak <span><?= $tulisan[5] ?></span>
     </p>
 </body>
+
 </html>
